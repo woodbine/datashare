a90ff17b7512431e83f03dbd0336b6210f09564e
blablupcom <o.volodin@yahoo.com>
2017-05-17 12:47:44 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,8 +9,9 @@ import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-#### FUNCTIONS 1.0
+#### FUNCTIONS 1.2
 
+import requests    #  import requests to validate url
 
 def validateFilename(filename):
     filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
@@ -50,7 +51,7 @@ def validateURL(url):
         else:
             ext = os.path.splitext(url)[1]
         validURL = r.getcode() == 200
-        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
+        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx', '/csv']
         return validURL, validFiletype
     except:
         print ("Error validating URL.")
@@ -123,17 +124,18 @@ for url in urls:
             title = restdata.select_one('title').text
             csvMth = title.split()[-2][:3]
             csvYr = title.split()[-1]
-            if "to" in title:
+            if "to " in title:
                 if 'January to March' in title:
                     csvMth = 'Q1'
-                if 'April to June' in title:
+                elif 'April to June' in title:
                     csvMth = 'Q2'
-                if 'July to September' in title:
+                elif 'July to September' in title:
                     csvMth = 'Q3'
-                if 'October to December' in title:
+                elif 'October to December' in title:
                     csvMth = 'Q4'
                 else:
                     csvMth = 'Q0'
+
             if 'September' in csvYr:
                 csvYr = '2015'
             csvMth = convert_mth_strings(csvMth.upper())
@@ -163,7 +165,14 @@ for url in urls:
             data.append([csvYr, csvMth, link, entity_id])
     if 'blackburn.gov.uk' in url:
         entity_id = 'E2301_BWDBC_gov'
+        proxy = urllib2.ProxyHandler({'http': 'http://176.126.245.23:3128'})
+        opener = urllib2.build_opener(proxy)
+        urllib2.install_opener(opener)
         html = urllib2.urlopen(url)
+        # headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 OPR/42.0.2393.94',
+        #            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+        # proxy = {'http':'http://176.126.245.23:3128'}
+        # page = requests.get(url, proxies=proxy, headers=headers)
         soup = BeautifulSoup(html, 'lxml')
         restdataset = soup.select('restschema')
         for restdata in restdataset:
@@ -171,7 +180,9 @@ for url in urls:
             title = restdata.select_one('title').text
             path_link = friendlyurl.text
             html = urllib2.urlopen(url+'/'+path_link)
+            # html = requests.get(url+'/'+path_link, proxies=proxy)
             soup = BeautifulSoup(html, 'lxml')
+            # soup = BeautifulSoup(html, 'lxml')
             restdataset = soup.select('restdataset')
             for restdata in restdataset:
                 link = restdata.select_one('friendlyurl').text.replace('/XML', '/csv')
